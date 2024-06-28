9bee9ca722628980ad4543c444634b3f501f798c
kaconk17 <harutonouciha@gmail.com>
2024-06-28 15:20:39 +0700

    db migration

---
+++ config/migration.js
@@ -0,0 +1,35 @@
+import pool from './connection.js';
+
+console.log('Starting...');
+pool.on('connect', ()=>{
+    console.log('koneksi berhasil');
+    pool.on('error', (err) => {
+        console.error(err);
+      });
+});
+
+const createUserTable = () => {
+    const sqlquery = 'CREATE TABLE IF NOT EXISTS tb_users (id UUID PRIMARY KEY,nama VARCHAR(100) NOT NULL,email VARCHAR(100) UNIQUE NOT NULL,password VARCHAR(100) NOT NULL, windows_id VARCHAR(100),created_at TIMESTAMP,updated_at TIMESTAMP)';
+    pool.query(sqlquery)
+    .then((res)=>{
+        console.log(res);
+        pool.end();
+    })
+    .catch((err)=>{
+        console.log(err);
+        pool.end();
+    });
+};
+
+const createChatTable = () => {
+    const sqlquery = 'CREATE TABLE IF NOT EXISTS tb_chat (id UUID PRIMARY KEY,chatroom_id VARCHAR(100) NOT NULL,pesan TEXT NOT NULL,posted_by VARCHAR(100) NOT NULL, created_at TIMESTAMP,updated_at TIMESTAMP)';
+    pool.query(sqlquery)
+    .then((res)=>{
+        console.log(res);
+        pool.end();
+    })
+    .catch((err)=>{
+        console.log(err);
+        pool.end();
+    });
+};
