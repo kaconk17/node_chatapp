51a82d70b3307c46b65afd2b118235caee882bef
kaconk <harutonouciha@gmail.com>
2024-07-17 21:14:54 +0700

    auth token

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-const { createServer } = require('http');
-const { fileURLToPath } = require('url');
-const { dirname, join } = require('path');
-const { Server } = require('socket.io');
+const { createServer } = require("http");
+const { fileURLToPath } = require("url");
+const { dirname, join } = require("path");
+const { Server } = require("socket.io");
 
-const { Socket } = require('dgram');
-const express = require('express');
+const { Socket } = require("dgram");
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
+const { log } = require("console");
 const server = http.createServer(app);
 const io = new Server(server);
 
-
-
-
 //const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname,'chat.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "chat.html"));
 });
-
-io.on('connection', async (Socket)=>{
-    console.log('a user connected');
-    Socket.on('chat', (msg)=>{
-        console.log('Message: '+msg);
-    });
-    Socket.on('disconnect', ()=>{
-        console.log('user disconnected');
-    });
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  console.log(token);
 });
-
-
+io.on("connection", async (Socket) => {
+  console.log("a user connected");
+  Socket.on("chat", (msg) => {
+    console.log("Message: " + msg);
+  });
+  Socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
