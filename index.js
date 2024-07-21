172f9e47da1b16e7b95ad4925f9392dc7958a835
kaconk <harutonouciha@gmail.com>
2024-07-21 21:55:28 +0700

    jwt middleware

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-const { createServer } = require("http");
-const { fileURLToPath } = require("url");
 const { dirname, join } = require("path");
 const { Server } = require("socket.io");
-
-const { Socket } = require("dgram");
 const express = require("express");
 const app = express();
 const http = require("http");
-const { log } = require("console");
+const bodyParser = require("body-parser");
 const server = http.createServer(app);
 const io = new Server(server);
+const port = 3000;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-//const __dirname = dirname(fileURLToPath(import.meta.url));
-
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+app.use("/static", express.static(join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "chat.html"));
 });
 io.use((socket, next) => {
   const token = socket.handshake.auth.token;
+  if (!token) {
+    console.log("Invalid token!");
+    return next(new Error("invalid token"));
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.user = decoded.data;
+    next();
+  } catch (error) {
+    return next(new Error("invalid token"));
+  }
+  // jwt.verify(token, process.env.SECRET, (err, decoded) => {
+  //   if (err) {
+  //     return next(new Error("invalid token"));
+  //   }
+  //   req.user = decoded.data;
+  //   next();
+  // });
   console.log(token);
+  //next();
 });
 io.on("connection", async (Socket) => {
   console.log("a user connected");
@@ -30,6 +53,6 @@ io.on("connection", async (Socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,6 +1,37 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
+const decode = (token, decoded) => {
+  try {
+    decoded = jwt.verify(token, process.env.SECRET);
+    // req.user = {
+    //   email: decoded.email,
+    //   user_id: decoded.user_id,
+    // };
+    // next();
+    return decoded;
+  } catch (error) {
+    return next(new Error("invalid Token"));
+  }
+};
 
-export const decode = (req, res, next) => {}
+const encode = async (email, id) => {
+  try {
+    const token = jwt.sign(
+      {
+        email,
+        user_id: id,
+      },
+      process.env.SECRET,
+      { expiresIn: "3d" },
+    );
+    return token;
+  } catch (error) {
+    return error;
+  }
+};
 
-export const encode = async (req, res, next) => {}
+module.exports = {
+  encode,
+  decode,
+};
